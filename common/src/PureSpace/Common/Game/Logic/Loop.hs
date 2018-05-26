59c2ea838d6e212726ac79a607a216f9511a4a88
Raveline <eraveline@gmail.com>
2018-05-26 12:21:46 +0200

    Add: communication between graphics and game loop

--- common/src/PureSpace/Common/Game/Logic/Loop.hs
+++ common/src/PureSpace/Common/Game/Logic/Loop.hs
@@ -22,6 +22,7 @@
 module PureSpace.Common.Game.Logic.Loop
   (
     logicLoop,
+    runLogic,
     Timer (..)
   )
   where
@@ -29,18 +30,15 @@ module PureSpace.Common.Game.Logic.Loop
 import           PureSpace.Common.Concurrent
 import           PureSpace.Common.Lens
 import           System.Clock
+import           PureSpace.Common.Game.Entity
+import           PureSpace.Common.Game.GameFSM
+import           PureSpace.Common.Prelude
 
 type Milliseconds = Integer
 type Microseconds = Integer
 type Nanoseconds  = Integer
 
 newtype Timer = Timer Integer
-
-class HasTimer t where
-  timer :: Lens' t Timer
-
-instance HasTimer Timer where
-  timer = id
 
 class HasTime t where
   time :: Lens' t Integer
@@ -51,12 +49,59 @@ instance HasTime Timer where
         g _ = Timer
     in lens f g
 
+data LoopState = LoopState Timer GameState
+
+instance HasGameState LoopState where
+  gameState =
+    let f (LoopState _ b) = b
+        g (LoopState a _) = LoopState a
+    in lens f g
+
+instance HasTime LoopState where
+  time =
+    let f (LoopState a _) = a ^. time
+        g (LoopState _ b) a = LoopState (Timer a) b
+    in lens f g
+
+initialLoopState :: (MonadIO m) => GameConfig -> m LoopState
+initialLoopState config =
+  liftIO $ LoopState <$> (Timer . toNanoSecs <$> getTime MonotonicRaw)
+                     <*> (pure . initial $ config)
+
+runLogic :: (MonadIO m) => TChan GameState -> GameConfig -> m ()
+runLogic sink config = do
+  firstState <- initialLoopState config
+  runReaderT (evalStateT (logicLoop sink) firstState) config
+
 logicLoop :: (MonadIO m,
               MonadState s m,
+              MonadReader r m,
               HasTime s,
-              HasTimer s)
-          => m ()
-logicLoop = tick *> liftIO (threadDelay . fromIntegral . milliToMicro $ 50) *> logicLoop
+              HasGameState s,
+              HasGridSize r,
+              HasGridDivision r,
+              HasGameConfig r) => TChan GameState -> m ()
+logicLoop sink = do
+    tick
+    newState <- use gameState
+    liftIO $ atomically (writeTChan sink newState)
+    liftIO (threadDelay . fromIntegral . milliToMicro $ 50)
+    logicLoop sink
+
+initial :: GameConfig -> GameState
+initial (GameConfig a b) =
+  GameState (createSpatialGrid a b (EntityShip <$> oneShips <> twoShips))
+    [PlayerState One [] oneShips 0 [],
+     PlayerState Two [] twoShips 0 []]
+  where
+    pc = ProjectileCaracteristics (ProjectileType Laser 2 6) 10 (V2 1500 1500)
+    sc = ShipCaracteristics (ShipType Fighter 100 75) pc 100 (V2 300 300) 1 (CircleRange 500)
+    oneShips = [Ship sc One 100 0 (V2 0 1000) (V2 0 0) 0,
+                Ship sc One 100 0 (V2 0 500) (V2 0 0) 0,
+                Ship sc One 100 0 (V2 0 0) (V2 0 0) 0]
+    twoShips = [Ship sc Two 100 0 (V2 1500 1000) (V2 0 0) 0,
+                Ship sc Two 100 0 (V2 1500 500) (V2 0 0) 0,
+                Ship sc Two 100 0 (V2 1500 0) (V2 0 0) 0]
 
 nanoToMilli :: Nanoseconds -> Milliseconds
 nanoToMilli = (`div` 1000000)
@@ -66,13 +111,21 @@ milliToMicro = (* 1000)
 
 tick :: (MonadIO m,
          MonadState s m,
+         MonadReader r m,
          HasTime s,
-         HasTimer s)
+         HasGameState s,
+         HasGridSize r,
+         HasGridDivision r,
+         HasGameConfig r)
      => m ()
 tick = do
   previous <- get
   now <- liftIO $ toNanoSecs <$> getTime MonotonicRaw
   let delta = nanoToMilli $ now - (previous ^. time)
-  modify $ timer .~ Timer now
-  -- liftIO $ putStrLn $ "Tick with delta " ++ show delta ++ " ms"
-
+  modify $ time .~ now
+  let ellapsedSeconds = fromInteger delta / 1000
+  liftIO $ print ellapsedSeconds
+  config <- ask
+  prevGameState <- use gameState
+  newState <- pure $ execState (runReaderT (updateGame ellapsedSeconds) config) prevGameState
+  gameState .= newState
